cc344556f7f15de1285163938c025c30b13aaab9
Federico Angelo Mor <federangelomor@gmail.com>
2024-02-21 20:53:52 +0100

    needed folder Fede for cmake conflict
    cmake usa i file nelle directory ettore/ecc, non sembra si possa generalizzare per più utenti

---
+++ Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Board.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Board.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Board_graph_algo.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Board_graph_algo.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Board_helper_functions.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Board_helper_functions.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Board_move.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Board_move.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Player.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Player.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Position.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Position.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/Tile.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/Tile.cpp.o"
+  "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/main.cpp" "/mnt/c/Users/feder/Desktop/Uni magistrale/Advanced programming for scientific computing/Quoridor-competition/Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Fede/cmake-build-debug/CMakeFiles/Quoridor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quoridor.dir/Board.cpp.o"
+  "CMakeFiles/Quoridor.dir/Board_graph_algo.cpp.o"
+  "CMakeFiles/Quoridor.dir/Board_helper_functions.cpp.o"
+  "CMakeFiles/Quoridor.dir/Board_move.cpp.o"
+  "CMakeFiles/Quoridor.dir/Player.cpp.o"
+  "CMakeFiles/Quoridor.dir/Position.cpp.o"
+  "CMakeFiles/Quoridor.dir/Tile.cpp.o"
+  "CMakeFiles/Quoridor.dir/main.cpp.o"
+  "Quoridor"
+  "Quoridor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quoridor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
